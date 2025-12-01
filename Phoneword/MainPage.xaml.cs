99f96dc8b13ac6b1bd13f4be364941fd780ff3c2
Mphiliseni <Mphilisenialexmhlongo@gmail.com>
2025-12-01 19:00:39 +0200

    Initial commit: Phoneword MAUI app with phone dialer functionality

---
+++ Phoneword/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace Phoneword;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	string? translatedNumber;
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+			CallButton.Text = $"Call {translatedNumber}";
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+			CallButton.Text = "Call";
+        }
+    }
+
+	private async void OnCall(object sender, EventArgs e)
+	{
+		if (await this.DisplayAlert(
+			"Dial a Number",
+			$"Would you like to call {translatedNumber}?",
+			"Yes",
+			"No"))
+		{
+			var dialer = PhoneDialer.Default;
+			if (dialer.IsSupported)
+			{
+				dialer.Open(translatedNumber);
+			}
+		}
+	}
+}
